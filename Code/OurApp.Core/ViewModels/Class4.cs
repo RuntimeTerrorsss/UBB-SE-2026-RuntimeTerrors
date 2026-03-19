b9021a468d614500e3da25fd16bdf26d6dc5895e
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-03-19 14:12:15 +0200

    classes

---
+++ Code/OurApp.Core/ViewModels/Class4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurApp.Core.ViewModels
+{
+    internal class Class4
+    {
+    }
+}
