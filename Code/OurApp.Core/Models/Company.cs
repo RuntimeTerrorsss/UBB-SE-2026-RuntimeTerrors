89935dfadc41d61d90510b16b7c2fa9c1a4f1fbc
Tea <teodoradiudea17@gmail.com>
2026-03-23 15:35:55 +0200

    Company model updated

--- Code/OurApp.Core/Models/Company.cs
+++ Code/OurApp.Core/Models/Company.cs
@@ -10,22 +10,27 @@ namespace OurApp.Core.Models
     {
         public string Name { get; set; }
         public string AboutUs { get; set; }
-        public string Pfp_url { get; set; }
-        public string Logo_url { get; set; }
+        public string ProfilePicturePath { get; set; }
+        public string CompanyLogoPath { get; set; }
         public string Location { get; set; }
         public string Email { get; set; }
+
+        //public List<Job> Jobs { get; set; } = new();
+        //public List<string> Collaborators { get; set; } = new();
+        //public MiniGame MiniGame { get; set; }
+
         public Company( string name, string aboutus, string pfp_url, string logo_url, string location, string email) {
             this.Name = name;
             this.AboutUs = aboutus;
-            this.Pfp_url = pfp_url;
-            this.Logo_url = logo_url;
+            this.ProfilePicturePath = pfp_url;
+            this.CompanyLogoPath = logo_url;
             this.Location = location;
             this.Email = email;
         }
 
         public override string ToString()
         {
-            return "Company: " + Name + "\n" + AboutUs + "\n" + Pfp_url + Logo_url + Location + Email;
+            return "Company: " + Name + "\n" + AboutUs + "\n" + ProfilePicturePath + CompanyLogoPath + Location + Email;
         }
     }
 }
