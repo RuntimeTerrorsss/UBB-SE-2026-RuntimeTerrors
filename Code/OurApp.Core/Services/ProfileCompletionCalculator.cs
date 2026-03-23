8229ae74ae159ef9a9476c527a58bad8271fed31
Tea <teodoradiudea17@gmail.com>
2026-03-23 20:26:27 +0200

    changed my mind

--- Code/OurApp.Core/Services/ProfileCompletionCalculator.cs
+++ Code/OurApp.Core/Services/ProfileCompletionCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OurApp.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,50 @@ using System.Threading.Tasks;
 
 namespace OurApp.Core.Services
 {
-    internal class ProfileCompletionCalculator
+    public class ProfileCompletionCalculator
     {
+        //Company has:
+        //    this.Name = name;
+        //    this.AboutUs = aboutus;
+        //    this.Pfp_url = pfp_url;
+        //    this.Logo_url = logo_url;
+        //    this.Location = location;
+        //    this.Email = email;
+
+        public (int percentage, List<string> remainingTasks) Calculate(Company company)
+        {
+            int total = 5;
+            int done = 0;
+            var tasks = new List<string>();
+
+            if (!string.IsNullOrEmpty(company.CompanyImagePath)) done++;
+            else tasks.Add("Upload company picture");
+
+            if (!string.IsNullOrEmpty(company.AboutUs)) done++;
+            else tasks.Add("Add company description");
+
+            if (company.Jobs.Count >= 5) done++;
+            else tasks.Add("Post at least 5 jobs");
+
+            if (company.Collaborators.Count >= 2) done++;
+            else tasks.Add("Add 2 collaborators");
+
+            if (IsMiniGameComplete(company)) done++;
+            else tasks.Add("Complete mini-game");
+
+            return ((done * 100) / total, tasks);
+        }
+
+        private bool IsMiniGameComplete(Company c)
+        {
+            var g = c.MiniGame;
+            if (g == null) return false;
+
+            return !string.IsNullOrEmpty(g.Struggle1) &&
+                   !string.IsNullOrEmpty(g.Struggle2) &&
+                   g.Responses1.Count == 3 &&
+                   g.Responses2.Count == 3 &&
+                   g.Feedbacks.Count == 3;
+        }
     }
 }
